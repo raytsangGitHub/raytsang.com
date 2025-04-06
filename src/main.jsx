1a0df9602c0a1c73dd055441c804eed13bebd210
Ray <raytsang21@gmail.com>
2025-04-05 22:18:36 -0700

    added react-router-dom package

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/raytsang.com/'>
       <Routes>
         <Route path="*" element={<App />} />
       </Routes>
