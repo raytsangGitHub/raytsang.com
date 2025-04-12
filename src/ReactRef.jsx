8173cf03f073725507e2663ab0884c219f63445c
Ray <raytsang21@gmail.com>
2025-04-12 08:03:38 -0700

    updated ReactRef.jsx

--- src/ReactRef.jsx
+++ src/ReactRef.jsx
@@ -9,6 +9,10 @@ function ReactReference()
                 <a href="https://react.dev/reference/react" target="_blank" rel="noopener noreferrer">
                     Click on React Reference Overview.
                 </a>
+                <br/>
+                <a href="https://www.geeksforgeeks.org/react/" target="-blank">
+                    React Tutorial.
+                </a>
             </p>
         </div>
     )
