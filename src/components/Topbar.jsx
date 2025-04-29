0aa0961e3374d073fb5892bd71982075ce916c1a
Ray <raytsang21@gmail.com>
2025-04-29 13:40:59 -0700

    fixed top bar for responsive design

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -18,7 +18,58 @@ function TopBar()
 
                 </ul>
             </nav>
+            {/* Mobile Navigation (Hamburger Menu) */}
+            <div className="md:hidden flex items-center">
+                <button className="text-white focus:outline-none">
+                    <i className="fas fa-bars text-2xl"></i> {/* You can use a hamburger icon */}
+                </button>
+            </div>
 
+            {/* Mobile Menu */}
+            <div className="md:hidden w-full absolute top-16 left-0 bg-gradient-to-r from-blue-500 via-purple-300 to-pink-200 z-10 hidden">
+                <ul className="flex flex-col space-y-6 p-6">
+                    <li>
+                        <Link
+                            to="/skills"
+                            className="cursor-pointer hover:underline hover:text-yellow-300 hover:drop-shadow-md transition duration-300"
+                        >
+                            Skills
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to="/Work-Experiences"
+                            className="cursor-pointer hover:underline hover:text-yellow-300 hover:drop-shadow-md transition duration-300"
+                        >
+                            Experiences
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to="/Projects"
+                            className="cursor-pointer hover:underline hover:text-yellow-300 hover:drop-shadow-md transition duration-300"
+                        >
+                            Projects
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to="Resume"
+                            className="cursor-pointer hover:underline hover:text-yellow-300 hover:drop-shadow-md transition duration-300"
+                        >
+                            Resume
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to="/ContactMe"
+                            className="cursor-pointer hover:underline hover:text-yellow-300 hover:drop-shadow-md transition duration-300"
+                        >
+                            Contact
+                        </Link>
+                    </li>
+                </ul>
+            </div>
         </header>
     );
 }
