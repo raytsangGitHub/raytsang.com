1da9fe0734d649b207aa09486430faba5dba11e5
Ray <raytsang21@gmail.com>
2025-07-01 15:55:45 -0700

    Fix the Go back home button link for NotFoundPage

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -5,7 +5,7 @@ function NotFoundPage()
             <h1 className="text-4xl font-bold mb-4">404</h1>
             <p className="text-lg text-yellow-800 mb-8">Page under construction...</p>
             <a
-                href="/"
+                href="./"
                 className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
             >
                 Go Back Home
